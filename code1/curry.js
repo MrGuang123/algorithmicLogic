14c85387722611013f3b7801f1ca0d41db5afb48
ytg <522125842@qq.com>
2022-03-28 18:50:56 +0800

    complete day1

--- code1/curry.js
+++ code1/curry.js
@@ -1,1 +1,29 @@
 // 函数柯里化
+function curry(func, ...params) {
+  // 获取传入函数的形参
+  const formalParams = func.length
+
+  return (...args) => {
+    // 收集实参
+    const useArguments = [...params, ...args]
+
+    // 如果实参不够继续柯里化
+    if (useArguments.length < formalParams) {
+      return curry(func, ...useArguments)
+    }
+
+    // 参数够了直接执行函数
+    return func(...useArguments)
+  }
+}
+
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1, 2, 3)); // 6, still callable normally
+console.log(curriedSum(1)(2, 3)); // 6, currying of 1st arg
+console.log(curriedSum(1)(2)(3)); // 6, full currying
--- code1/lc-989.js
+++ code1/lc-989.js
@@ -1,1 +1,27 @@
 // 数组形式的整数加法
+var addToArrayForm = function (num, k) {
+  if (!Array.isArray(num) || typeof k !== 'number') {
+    return false
+  }
+
+  let kCopy = k
+  const result = []
+  // 数组从最后一位开始加k，大于10再和数组倒数第二位相加，直到数组遍历完成或者k为0
+  // 数组的push，reverse的时间要小于unshift时间
+  for (let i = num.length - 1; i >= 0 || kCopy; i--) {
+    let current = num[i] || 0
+
+    if (kCopy) {
+      current = kCopy + current
+      kCopy = (current - current % 10) / 10
+    }
+
+    result.push(current % 10)
+  }
+
+  return result.reverse()
+};
+
+addToArrayForm([2, 1, 5], 806)
+// addToArrayForm([1, 2, 0, 0], 34)
+// addToArrayForm([1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516)
