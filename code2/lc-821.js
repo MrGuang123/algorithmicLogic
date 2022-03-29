10ffcaa9d369a60555476980a140357f07048298
tingguang.yan <522125842@qq.com>
2022-03-29 22:36:03 +0800

    day2 algorithmic complete

--- code2/lc-821.js
+++ code2/lc-821.js
@@ -1,1 +1,17 @@
 // 字符的最短距离
+var shortestToChar = function (s, c) {
+  const cIndexs = []
+  const result = []
+  for (let i = 0; i < s.length; i++) {
+    s[i] === c && cIndexs.push(i)
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    result.push(Math.min(...cIndexs.map(ci => Math.abs(i - ci))))
+  }
+
+  return result;
+};
+
+shortestToChar('loveleetcode', 'e')
+shortestToChar('aaab', 'b')
