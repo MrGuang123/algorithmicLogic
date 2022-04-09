8d087a3bb7c87b6450136534510f0ebd74b91987
tingguang.yan <522125842@qq.com>
2022-04-10 00:07:50 +0800

    day7 algorithmic complete

--- code7/lc-61.js
+++ code7/lc-61.js
@@ -1,1 +1,30 @@
 // https://leetcode-cn.com/problems/rotate-list/
+
+var rotateRight = function (head, k) {
+  if (k === 0 || !head || !head.next) {
+    return head
+  }
+
+  let listCount = 1
+  let current = head
+  while (current.next) {
+    current = current.next
+    listCount++
+  }
+
+  let rotateNum = listCount - k % listCount
+  if (rotateNum === listCount) {
+    return head
+  }
+
+  current.next = head
+  while (rotateNum) {
+    current = current.next
+    rotateNum--
+  }
+
+  const result = current.next
+  current.next = null
+
+  return result
+}
