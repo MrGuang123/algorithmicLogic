1289a1b9f3bab250a2cf83b369f0fea051b44ecd
tingguang.yan <522125842@qq.com>
2022-04-09 19:43:40 +0800

    day8 algorithmic comments

--- code8/lc-24.js
+++ code8/lc-24.js
@@ -11,6 +11,10 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// 创建一个新的链表result，prev表示当前到达的节点，
+// 当前首先到达新创建的节点，然后next指向传入的链表
+// 判断如果当前到达的下两个节点有值，则进行交换，否则没有值或者只有一个值直接退出循环
+// 拿到要交换的两个值，注意赋值顺序，最后更新当前到达的节点为替换后处于后面位置的节点
 var swapPairs = function (head) {
   let result = new ListNode()
   let prev = result
