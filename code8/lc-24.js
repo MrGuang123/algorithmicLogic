74b7e953b8709813c4f819858d54a70e7ff854ac
tingguang.yan <522125842@qq.com>
2022-04-09 19:36:39 +0800

    day8 algorithmic complete

--- code8/lc-24.js
+++ code8/lc-24.js
@@ -1,1 +1,30 @@
 // https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  let result = new ListNode()
+  let prev = result
+  prev.next = head
+
+  while (prev.next && prev.next.next) {
+    let node1 = prev.next
+    let node2 = node1.next
+
+    prev.next = node2
+    node1.next = node2.next
+    node2.next = node1
+    prev = node1
+  }
+
+  return result.next
+};
