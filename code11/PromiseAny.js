8168e4f0840f0b4f47aee77e763bc72e44fc2c74
tingguang.yan <522125842@qq.com>
2022-04-13 22:49:22 +0800

    day11 logic complete

--- code11/PromiseAny.js
+++ code11/PromiseAny.js
@@ -1,1 +1,31 @@
 // https://bigfrontend.dev/zh/problem/implement-Promise-any
+/**
+ * @param {Array<Promise>} promises
+ * @return {Promise}
+ */
+function any(promises) {
+  return new Promise((resolve, reject) => {
+    promises = Array.isArray(promises) ? promises : []
+    const length = promises.length
+    const errorList = []
+
+    if (length === 0) {
+      return new AggregateError('No Promise in there')
+    }
+
+    promises.forEach((promise, index) => {
+      promise.then(res => {
+        resolve(res)
+      }).catch(error => {
+        errorList[index] = error
+
+        if (errorList.length === promises.length) {
+          reject(new AggregateError(
+            'No Promise in Promise.any was resolved',
+            errorList
+          ))
+        }
+      })
+    })
+  })
+}
