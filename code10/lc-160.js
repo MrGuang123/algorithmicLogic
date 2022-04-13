4408a92797236a1545d3074f3b77682441dba72e
ytg <522125842@qq.com>
2022-04-13 10:49:06 +0800

    day10 algorithmic complete

--- code10/lc-160.js
+++ code10/lc-160.js
@@ -1,1 +1,20 @@
 // https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+var getIntersectionNode = function (headA, headB) {
+  const record = new Set()
+  let alinkedList = headA
+  let bLinkedList = headB
+  while (alinkedList) {
+    record.add(alinkedList)
+    alinkedList = alinkedList.next
+  }
+
+  while (bLinkedList) {
+    if (record.has(bLinkedList)) {
+      return bLinkedList
+    }
+
+    bLinkedList = bLinkedList.next
+  }
+
+  return null
+};
