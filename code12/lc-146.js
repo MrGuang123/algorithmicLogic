481eb836f2bf703a8a4da64bf21b6bfa494b0a1f
tingguang.yan <522125842@qq.com>
2022-04-14 23:35:15 +0800

    day12 algorithmic complete

--- code12/lc-146.js
+++ code12/lc-146.js
@@ -1,1 +1,53 @@
 // https://leetcode-cn.com/problems/lru-cache/
+/**
+ * @param {number} capacity
+ */
+// Map是有序的
+var LRUCache = function (capacity) {
+  this.cacheLength = capacity
+  this.cache = new Map()
+};
+
+/** 
+* @param {number} key
+* @return {number}
+*/
+LRUCache.prototype.get = function (key) {
+  if (this.cache.has(key)) {
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+
+    return value
+  } else {
+    return -1
+  }
+};
+
+/** 
+* @param {number} key 
+* @param {number} value
+* @return {void}
+*/
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) {
+    this.cache.delete(key)
+  }
+
+  this.cache.set(key, value)
+
+  if (this.cache.size > this.cacheLength) {
+    this.cache.delete(this.cache.keys().next().value)
+  }
+};
+
+var obj = new LRUCache(2)
+obj.put(1, 1)
+obj.put(2, 2)
+var param_1 = obj.get(1)
+obj.put(3, 3)
+var param_1 = obj.get(2)
+obj.put(4, 4)
+var param_1 = obj.get(1)
+var param_1 = obj.get(3)
+var param_1 = obj.get(4)
