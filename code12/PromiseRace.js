fb78d28baa3e1c7450316c858d369bc54409407b
tingguang.yan <522125842@qq.com>
2022-04-16 23:46:38 +0800

    day12 logic complete

--- code12/PromiseRace.js
+++ code12/PromiseRace.js
@@ -1,1 +1,16 @@
 // https://bigfrontend.dev/zh/problem/implement-Promise-race
+/**
+ * @param {Array<Promise>} promises
+ * @return {Promise}
+ */
+function race(promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(res => {
+        resolve(res)
+      }).catch(error => {
+        reject(error)
+      })
+    }
+  })
+}
