267e6170f46dd0b9a4a348105e37906b6aa4b72d
tingguang.yan <522125842@qq.com>
2022-04-20 01:05:29 +0800

    day15 algorithmic complete

--- code15/lc-129.js
+++ code15/lc-129.js
@@ -1,1 +1,22 @@
 // https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+  return dfs(root, 0)
+};
+
+function dfs(node, preSum) {
+  if (node === null) {
+    return 0
+  }
+
+  const sum = node.val + preSum * 10
+  if (node.left === null && node.right === null) {
+    return sum
+  } else {
+    return dfs(node.left, sum) + dfs(node.right, sum)
+  }
+}
