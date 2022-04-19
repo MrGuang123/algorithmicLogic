21d8f5f97d8e4a1e537f25358dece92c9d6011e0
ytg <522125842@qq.com>
2022-04-19 10:16:51 +0800

    day14 algorithmic complete

--- code14/lc-100.js
+++ code14/lc-100.js
@@ -1,1 +1,18 @@
 // https://leetcode-cn.com/problems/same-tree/
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (p == null && q == null) {
+    return true
+  } else if (p == null || q == null) {
+    return false
+  } else if (p.val != q.val) {
+    return false
+  } else {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  }
+};
