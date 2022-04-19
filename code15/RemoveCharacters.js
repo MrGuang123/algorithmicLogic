f460877805cd613f14e33ddcab04e4fafcf96142
tingguang.yan <522125842@qq.com>
2022-04-20 01:10:40 +0800

    day15 logic complete

--- code15/RemoveCharacters.js
+++ code15/RemoveCharacters.js
@@ -1,1 +1,15 @@
 // https://bigfrontend.dev/zh/problem/remove-characters
+
+/**
+ * @param {string} input
+ * @returns string
+ */
+function removeChars(input) {
+  let withoutB = input.replaceAll('b', '')
+
+  while (withoutB.includes('ac')) {
+    withoutB = withoutB.replaceAll('ac', '')
+  }
+
+  return withoutB
+}
