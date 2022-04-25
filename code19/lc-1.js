c715fa11e3adfbcd00cf1be40df2e8874016db32
ytg <522125842@qq.com>
2022-04-25 09:54:15 +0800

    day19 algorithmic complete

--- code19/lc-1.js
+++ code19/lc-1.js
@@ -1,1 +1,19 @@
 // https://leetcode-cn.com/problems/two-sum
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const difference = target - nums[i];
+    if (map.has(difference)) {
+      return [map.get(difference), i]
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+
+  return null;
+};
