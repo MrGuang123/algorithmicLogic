95d776d6ac722a8e2c85f259e9ba611fe3fc4f82
ytg <522125842@qq.com>
2022-04-25 10:28:53 +0800

    day19 logic complete

--- code19/Compose.js
+++ code19/Compose.js
@@ -1,8 +1,28 @@
 // function compose(){
 //   //请实现
 // }
-// const multiply20 = (price) => price * 20;
-// const divide100 = (price) => price / 100;
-// const normalizePrice = (price) => price.toFixed(2);
-// const discount = compose(normalizePrice, divide100, multiply20);
-// discount(200.0); //40.00
+
+function compose(...args) {
+  for (let i = 0, len = args.length; i < len; i++) {
+    if (typeof args[i] !== 'function') {
+      return false
+    }
+  }
+
+  return (price) => {
+    let sum = price;
+
+    while (args.length) {
+      const fn = args.pop()
+      sum = fn(sum)
+    }
+
+    return sum
+  }
+}
+
+const multiply20 = (price) => price * 20;
+const divide100 = (price) => price / 100;
+const normalizePrice = (price) => price.toFixed(2);
+const discount = compose(normalizePrice, divide100, multiply20);
+console.log(discount(200.0)); //40.00
