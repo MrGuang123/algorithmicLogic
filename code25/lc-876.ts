c590720d738946298b2f0bd1bdc0ac7d14082358
ytg <522125842@qq.com>
2022-05-05 21:18:47 +0800

    day25 algorithmic complete

---
+++ code25/lc-876.ts
@@ -0,0 +1,31 @@
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+function middleNode(head: ListNode | null): ListNode | null {
+  let slow = head
+  let fast = head
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+};
+
+// function middleNode(head: ListNode | null): ListNode | null {
+//     const list = [head]
+
+//     while(head.next) {
+//         list.push(head.next)
+//         head = head.next
+//     }
+
+//     return list[Math.floor(list.length / 2)]
+// };
