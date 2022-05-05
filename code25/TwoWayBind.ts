019df2fac7e3f7703f9ee16d97267ce0f6cebaf8
ytg <522125842@qq.com>
2022-05-05 21:23:55 +0800

    day25 logic complete

--- code25/TwoWayBind.ts
+++ code25/TwoWayBind.ts
@@ -1,1 +1,10 @@
 // https://bigfrontend.dev/zh/problem/two-way-binding
+
+function model(state: { value: string }, element: HTMLInputElement) {
+  element.value = state.value
+
+  Object.defineProperty(state, 'value', {
+    get: () => element.value,
+    set: value => element.value = value
+  })
+}
